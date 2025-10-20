80ec796626ec420ddac7ce62f5b4c967f10a0d9c
nianien <nianien@gmail.com>
2025-10-20 22:02:48 +1100

    init

--- ci/env.sh
+++ ci/env.sh
@@ -6,7 +6,7 @@ set -euo pipefail
 # =========================================================
 export AWS_PAGER="" # 防止 CLI 卡分页
 export AWS_PROFILE="${AWS_PROFILE:-nianien}"
-export AWS_REGION="${AWS_REGION:-us-east-1}"
+export AWS_REGION="${AWS_REGION:-ap-southeast-2}"
 export AWS_ACCOUNT_ID="${AWS_ACCOUNT_ID:-297997107448}"
 
 # =========================================================
