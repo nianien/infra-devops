f546eadafc79ac399bcddc3a9bb7d596637ce02d
nianien <nianien@gmail.com>
2025-10-21 20:31:16 +1100

    init

--- ci/env.sh
+++ ci/env.sh
@@ -8,9 +8,3 @@ export AWS_PAGER="" # 防止 CLI 卡分页
 export AWS_PROFILE="${AWS_PROFILE:-nianien}"
 export AWS_REGION="${AWS_REGION:-ap-southeast-2}"
 export AWS_ACCOUNT_ID="${AWS_ACCOUNT_ID:-297997107448}"
-
-# =========================================================
-# 兼容性保留（可选）
-# =========================================================
-export CLUSTER="${CLUSTER:-demo-cluster}"
-export NAMESPACE_NAME="${NAMESPACE_NAME:-test.local}"
