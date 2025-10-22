898f8e0be575ee01c746e92d482a314441319979
nianien <nianien@gmail.com>
2025-10-22 15:19:54 +1100

    实用脚本

--- ci/env.sh
+++ ci/env.sh
@@ -4,7 +4,6 @@ set -euo pipefail
 # =========================================================
 # AWS 认证配置
 # =========================================================
-export AWS_PAGER="" # 防止 CLI 卡分页
 export AWS_PROFILE="${AWS_PROFILE:-nianien}"
 export AWS_REGION="${AWS_REGION:-ap-southeast-2}"
 export AWS_ACCOUNT_ID="${AWS_ACCOUNT_ID:-297997107448}"
